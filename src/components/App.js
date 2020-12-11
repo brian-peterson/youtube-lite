f47e405c55002847a92c2f4fc4ce1c01b9c680b5
brian-peterson <bpete456@gmail.com>
2020-12-11 00:41:10 -0600

    added some styling and a default video to the player

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,10 @@ import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
     state = {videos: [], selectedVideo: null };
+
+    componentDidMount() {
+        this.onTermSubmit('ufos');
+    }
     
     onTermSubmit = async term => {
         // ---Asynchronous API request---
@@ -15,7 +19,8 @@ class App extends React.Component {
             }
         });
         // ------------------------------
-        this.setState({videos: response.data.items})
+        this.setState({videos: response.data.items,
+             selectedVideo: response.data.items[0]})
     };
 
     onVideoSelect = video => {
@@ -26,8 +31,16 @@ class App extends React.Component {
         return (
             <div className="ui container">
             <SearchBar onTermSubmit={this.onTermSubmit} />
-            <VideoDetail video={this.state.selectedVideo} />
-            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                    <VideoDetail video={this.state.selectedVideo} />
+                    </div>
+                    <div className="five wide column">
+                     <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                     </div>
+                </div>
+            </div>
             </div>
         )
     }
